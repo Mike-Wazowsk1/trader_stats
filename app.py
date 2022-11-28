e48eadc17ae08ec38454687b99271b43aecb4589
Mike-Wazowsk1 <exectai@gmail.com>
2022-11-28 08:29:42 +0300

    add balance info

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-           
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
