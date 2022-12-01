7d9ef132f40a8a3b8e194548bee58b3e38587ef2
Mike-Wazowsk1 <exectai@gmail.com>
2022-12-01 04:15:46 +0300

    fix

--- app.py
+++ app.py
@@ -3,34 +3,67 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from generator import PDF
+import pandas as pd
+import os
 
 UPLOAD_FOLDER = 'uploads'
+TMP_FOLDER = 'tmp'
 ALLOWED_EXTENSIONS = {'csv'}
 
+def save_data(name):
+    print(name)
+    df = pd.read_csv(os.path.join(app.config['TMP_FOLDER'], name),names=['date','open','high','low','close','volume'],parse_dates=['date'],header=0)
+    new_name = name.split(".")[0]+ '.parquet'
+    df.to_parquet('data/'+new_name)
+    flash("Loaded")
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['TMP_FOLDER'] = TMP_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)  # type: ignore
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            if 'data' not in request.files:
+                return render_template('index.html')
+        else:
+            file = request.files['file']
+            data = request.files['data']
+            if file.filename == '':
+                if data.filename == '':
+                    render_template('index.html')
+                else:
+                    if data and allowed_file(data.filename):
+                        data_filename = secure_filename(data.filename) # type: ignore
+                        if not os.path.exists(app.config['TMP_FOLDER']):
+                            os.makedirs(app.config['TMP_FOLDER'])
+                            data.save(os.path.join(app.config['TMP_FOLDER'], data_filename))
+                            save_data(data_filename)
+
+                        else:
+                            data.save(os.path.join(app.config['TMP_FOLDER'], data_filename))
+                            save_data(data_filename)
+
+            if file and allowed_file(file.filename):
+                if data and allowed_file(data.filename):
+                    data_filename = secure_filename(data.filename) # type: ignore
+                    if not os.path.exists(app.config['TMP_FOLDER']):
+                        os.makedirs(app.config['TMP_FOLDER'])
+                        data.save(os.path.join(app.config['TMP_FOLDER'], data_filename))
+                        save_data(data_filename)
+                    else:
+                        data.save(os.path.join(app.config['TMP_FOLDER'], data_filename))
+                        save_data(data_filename)
+
+                filename = secure_filename(file.filename)  # type: ignore
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                return redirect(url_for('download_file', name=filename))
     return render_template('index.html')
 @app.route('/uploads/<name>')
 def download_file(name):
@@ -43,4 +76,6 @@ def download_file(name):
     new_path = name[:-4] +".pdf"
     return send_from_directory(app.config["UPLOAD_FOLDER"], new_path)  # type: ignore
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(host='0.0.0.0')
